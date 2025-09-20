831bcd919338d86fc28942f0b0b11d194d0fed21
Laud Zion Cascalla <93295452+Sayonnn@users.noreply.github.com>
2025-09-20 22:05:50 +0800

    refactor: fix route index.js

--- backend/app/configs/index.js
+++ backend/app/configs/index.js
@@ -3,12 +3,8 @@ dotenv.config();
 
 export const config = {
     port: process.env.PORT || 5000, 
-    db:{
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: process.env.DB_PORT
+    db: {
+        mongo_uri: process.env.MONGO_URI
     },
     jwt:{
         secret: process.env.JWT_SECRET,
@@ -16,7 +12,6 @@ export const config = {
         refresh_token_expires_in: process.env.JWT_REFRESH_TOKEN_EXPIRES_IN,
         algorithm: process.env.JWT_ALGORITHM
     },
-    redis:{},
     mail:{
         host: process.env.MAIL_HOST,
         port: process.env.MAIL_PORT,
